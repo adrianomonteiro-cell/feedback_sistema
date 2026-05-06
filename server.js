250ae70b1339d61ba9f74d4c8d2aedb732677db7
Adriano Monteiro <adriano22monteiro@gmail.com>
2026-05-06 15:10:40 -0300

    fix porta railway

--- server.js
+++ server.js
@@ -54,5 +54,5 @@ app.post('/usuarios', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Servidor rodando');
+  console.log('Servidor rodando na porta ' + PORT);
 });
