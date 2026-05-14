4105ec3ab6be830393a99016ab2c5609595cd86d
Adriano Monteiro <adriano22monteiro@gmail.com>
2026-05-14 15:23:47 -0300

    debug backend

--- server.js
+++ server.js
@@ -1,49 +1,48 @@
-rconsole.log('INICIANDO SERVIDOR...');
+console.log('INICIANDO');
 
 require('dotenv').config();
-console.log('DOTENV OK');
 
 const express = require('express');
-console.log('EXPRESS OK');
-
 const cors = require('cors');
-console.log('CORS OK');
-
 const { Pool } = require('pg');
-console.log('PG OK');
+
+console.log('MODULOS OK');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-console.log('CRIANDO CONEXAO BANCO...');
+console.log('CRIANDO POOL');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
   .then(() => {
-    console.log('BANCO CONECTADO');
+    console.log('BANCO OK');
   })
-  .catch(err => {
-    console.error('ERRO BANCO:', err);
+  .catch((err) => {
+    console.error('ERRO BANCO');
+    console.error(err);
   });
 
 app.get('/', (req, res) => {
-  res.send('API funcionando 🚀');
+  res.send('API funcionando');
 });
 
 app.get('/health', (req, res) => {
-  res.json({ status: 'ok' });
+  res.json({
+    status: 'online'
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('SERVIDOR RODANDO NA PORTA ' + PORT);
+  console.log('PORTA ' + PORT);
 });
